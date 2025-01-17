c4fbcc7e757ca939b13b3ff00d5e24f95d8fbeed
Tobi-013 <156852802+Tobi-013@users.noreply.github.com>
2025-01-16 23:34:07 -0700

    Prep 2 assignment : grading scale

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int x = int.Parse(percentage);
+        string letter = "";
+        if(x >= 90)
+        {
+            letter = "A";
+        }
+        else if (x >= 80)
+        {
+            letter = "B";
+        }
+        else if ( x >= 70)
+        {
+            letter = "C";
+        }
+        else if ( x >= 60)
+        {
+            letter = "D";
+        }
+        else if ( x < 60)
+        {
+           letter = "F";
+        }
+        if (x>=70)
+        {
+            Console.WriteLine($"Your grade is, {letter}");
+            Console.Write("Congratulations you passed!");
+        }
+        else 
+        {   Console.WriteLine($"Your grade is, {letter}");
+            Console.Write("You Failed, Try again next time.");
+        }
+
     }
 }
