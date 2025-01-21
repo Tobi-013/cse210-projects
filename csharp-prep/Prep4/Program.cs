0ee4cb39c07b718ce12766b2666b3a48e96fd053
Tobi-013 <156852802+Tobi-013@users.noreply.github.com>
2025-01-21 16:45:45 -0700

    PREP 3-5 codes

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        int number = -1;
+        List<int> ListOfNumbers = new List<int>();
+    while  (number != 0)
+    {
+        Console.WriteLine("Enter number: ");
+        string x = Console.ReadLine(); 
+        number = int.Parse(x);
+
+        ListOfNumbers.Add(number);
+
     }
+    int sum = 0 ;
+     int largestNum = ListOfNumbers[0];
+    foreach (int num in ListOfNumbers)
+    {
+        if(num > largestNum)
+    {
+        largestNum = num;
+    } 
+        sum += num;
+    }
+    int length =ListOfNumbers.Count;
+        float average = sum/length;
+
+    Console.WriteLine($"The sum is: {sum}");
+    Console.WriteLine($"The average is: {average}");
+    Console.WriteLine($"The largest number: {largestNum}");
+     
+    }
+        
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+         DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squaredNumber);
     }
-}
+    static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+    static string PromptUserName()
+    {
+        Console.WriteLine("What is your name? ");
+        string name = Console.ReadLine();
+        
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("What is your favourite number? ");
+        int FavNum = int.Parse(Console.ReadLine());
+
+        return FavNum;
+    }
+
+    static int SquareNumber(int FavNum)
+    {
+        int square = FavNum * FavNum;
+
+         return square;
+    }
+    static void DisplayResult(string name,int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+}   
